bd56edaf5428972d0c9a300a54f9313c5fd74f7d
profmayck <85135029+profmayck@users.noreply.github.com>
2024-06-03 18:36:52 -0300

    Add files via upload

---
+++ exercicios-de-fixacao/exercicios.sql
@@ -0,0 +1,19 @@
+# 1. Liste todos os títulos dos livros junto com os nomes dos seus autores.
+
+# 2. Liste todos os títulos dos livros junto com suas categorias.
+
+# 3. Liste todos os títulos dos livros junto com os nomes das editoras e os idiomas.
+
+# 4. Liste todos os títulos dos livros disponíveis para empréstimo junto com seus autores.
+
+# 5. Liste todos os títulos dos livros na categoria "Fantasia" junto com seus autores.
+
+# 6. Conte quantos livros cada autor tem na biblioteca.
+
+# 7. Liste os títulos dos livros, o número de páginas e os nomes das editoras.
+
+# 8. Liste os títulos dos livros publicados entre 1950 e 2000 junto com seus autores.
+
+# 9. Liste os títulos dos livros que têm mais de um autor.
+
+# 10. Liste os títulos dos livros, nomes das editoras, categorias e idiomas.
---
+++ exercicios-de-fixacao/normalizacao.sql
@@ -0,0 +1,100 @@
+## O comando USE é por sua conta.
+
+CREATE TABLE categoria(
+	id tinyint unsigned primary key auto_increment,
+    nome varchar(30) not null
+);
+
+INSERT INTO categoria(nome)
+SELECT DISTINCT categoria FROM livros order by categoria;
+
+SELECT * FROM categoria;
+
+ALTER TABLE livros
+ADD id_categoria tinyint unsigned;
+
+ALTER TABLE livros
+ADD CONSTRAINT FOREIGN KEY (id_categoria)
+REFERENCES categoria(id);
+
+SELECT * FROM livros;
+
+UPDATE livros l
+SET id_categoria = ( SELECT id FROM categoria c WHERE c.nome = l.categoria);
+
+ALTER TABLE livros
+DROP COLUMN categoria;
+
+CREATE TABLE idioma(
+	id tinyint unsigned primary key auto_increment,
+    nome varchar(30) not null
+);
+
+INSERT INTO idioma(nome)
+SELECT DISTINCT idioma FROM livros order by idioma;
+
+
+ALTER TABLE livros
+ADD id_idioma tinyint unsigned;
+
+ALTER TABLE livros
+ADD CONSTRAINT FOREIGN KEY (id_idioma)
+REFERENCES idioma(id);
+
+UPDATE livros l
+SET id_idioma = ( SELECT id FROM idioma i WHERE i.nome = l.idioma);
+
+ALTER TABLE livros
+DROP COLUMN idioma;
+
+CREATE TABLE Editora(
+	id tinyint unsigned primary key auto_increment,
+    nome varchar(50) not null
+);
+
+INSERT INTO Editora(nome)
+SELECT DISTINCT Editora FROM livros order by Editora;
+
+SELECT * FROM Editora;
+
+
+ALTER TABLE livros
+ADD id_editora tinyint unsigned;
+
+ALTER TABLE livros
+ADD CONSTRAINT FOREIGN KEY (id_editora)
+REFERENCES editora(id);
+
+UPDATE livros l
+SET id_editora = ( SELECT id FROM editora i WHERE i.nome = l.editora);
+
+ALTER TABLE livros
+DROP COLUMN editora;
+
+CREATE TABLE Autor(
+	id tinyint unsigned primary key auto_increment,
+    nome varchar(50) not null
+);
+
+INSERT INTO Autor(nome)
+SELECT DISTINCT Autor FROM livros order by Autor;
+
+SELECT * FROM Autor;
+
+DROP TABLE Autor_Livro;
+CREATE TABLE Autor_Livro(
+	id_livro int,
+    id_autor tinyint unsigned,
+    CONSTRAINT PRIMARY KEY (id_livro, id_autor),
+    CONSTRAINT `FK_LIVRO` FOREIGN KEY (id_livro)
+		REFERENCES livros(id),
+	CONSTRAINT `FK_AUTOR` FOREIGN KEY (id_autor)
+		REFERENCES autor(id)
+);
+INSERT INTO Autor_Livro
+SELECT livros.id, autor.id
+FROM livros
+INNER JOIN autor ON autor.nome = livros.autor;
+
+ALTER TABLE livros
+DROP COLUMN autor;
